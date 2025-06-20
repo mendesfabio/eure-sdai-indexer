5557ea9f5ebb53db383318eb979e2964d80eb0c0
mendesfabio <ms.fabiomendes@gmail.com>
2025-06-20 13:43:49 +0100

    add balancer abis

---
+++ abis/RateProviderAbi.ts
@@ -0,0 +1,9 @@
+export const RateProviderAbi = [
+  {
+    inputs: [],
+    name: "getRate",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+] as const;
