f7fc96e3c87cbeaebd1867b91ec5cd9e39efe5c3
mendesfabio <ms.fabiomendes@gmail.com>
2025-06-23 10:45:02 +0100

    filter pool by topic

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,6 +1,7 @@
 import { createConfig } from "ponder";
 
 import { VaultAbi } from "./abis/VaultAbi";
+import { POOL_ID } from "./src/utils/const";
 
 export default createConfig({
   chains: { gnosis: { id: 100, rpc: process.env.PONDER_RPC_URL_100 } },
@@ -11,6 +12,26 @@ export default createConfig({
       startBlock: 30274134, // EURe/sDAI pool created (https://gnosisscan.io/tx/0x8920d9efac5b1427bbc8e1572b98afe0987aa27fe6e884602ef7757b4a1be8fe)
       endBlock: 39440898, // Cache duration fixed (https://gnosisscan.io/tx/0xa7e848092499d4b29c08daebd80f15d657609a976876b3b026c724d9705a1107)
       chain: "gnosis",
+      filter: [
+        {
+          event: "PoolRegistered",
+          args: {
+            poolId: POOL_ID,
+          },
+        },
+        {
+          event: "PoolBalanceChanged",
+          args: {
+            poolId: POOL_ID,
+          },
+        },
+        {
+          event: "Swap",
+          args: {
+            poolId: POOL_ID,
+          },
+        },
+      ],
     },
   },
 });
